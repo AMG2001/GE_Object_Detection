9f7bb6022691621aff51ab7357d9d0af4b4120c5
Mohamad amgad elsaid <mohamadamgad09@gmail.com>
2023-07-13 23:29:27 +0300

    google mlkit solution added , and worked
    we used google ml solution and give us satisfying results , now we need to implement our custom model to perform required task .

--- ge_camera_app/lib/main.dart
+++ ge_camera_app/lib/main.dart
@@ -1,11 +1,11 @@
-
 import 'package:flutter/material.dart';
-import 'package:ge_camera_app/object_detection_tensor_flow.dart';
+import 'package:ge_camera_app/object_detection_google_ml.dart';
 
 void main() {
-  runApp(MaterialApp(
-    title: "Camera Detection App",
-    home:ObjectDetectionScreen() ,
-  ));
+  runApp(
+    MaterialApp(
+      title: "Camera Detection App",
+      home: ObjectDetectorView(),
+    ),
+  );
 }
-
